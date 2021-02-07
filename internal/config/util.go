8fc6d51259b14e55e9c10f999e8055739228cff9
Hamid Elaosta <hamid@2byt.es>
2021-02-07 11:54:48 +0000

    New themes Add string formatter for expiry Minor code optimisations

--- internal/config/util.go
+++ internal/config/util.go
@@ -1,6 +1,12 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+)
 
 // FormatBaseAddress returns the public base address of the server given the server config
 // it ignores the port if it's default for the given protocol
@@ -19,4 +25,62 @@ func (c *Config) FormatBaseAddress() string {
 	}
 
 	return base + "/"
+}
+
+type timeString struct {
+	b strings.Builder
+}
+
+func (ts *timeString) append(word string, number uint64) {
+
+	if number <= 0 {
+		return
+	}
+
+	if ts.b.Len() != 0 {
+		ts.b.WriteString(", ")
+	}
+
+	//fmt.Sprintf("%d %s", number, word)
+	ts.b.WriteString(strconv.FormatUint(number, 10))
+	ts.b.WriteString(" ")
+	ts.b.WriteString(word)
+	if number > 1 {
+		ts.b.WriteString("s")
+	}
+}
+
+// FormatTime provides a human readable time string given a time.Duration
+func FormatTime(t time.Duration) string {
+	if t <= 0 {
+		return "∞"
+	}
+
+	var seconds, secondsCarried float64 = 0, 0
+	if t.Seconds() < 60 {
+		secondsCarried = t.Seconds()
+	} else {
+		secondsCarried = math.Mod(t.Seconds(), 60)
+		seconds = t.Seconds() - secondsCarried
+	}
+
+	mins := seconds / 60
+	minsCarried := math.Mod(seconds/60, 60)
+
+	hours := (mins - minsCarried) / 60
+	hoursCarried := math.Mod(hours, 24)
+
+	days := (hours - hoursCarried) / 24
+	daysCarried := math.Mod(days, 365)
+
+	years := (days - daysCarried) / 365
+
+	ts := timeString{}
+	ts.append("year", uint64(years))
+	ts.append("day", uint64(daysCarried))
+	ts.append("hour", uint64(hoursCarried))
+	ts.append("minute", uint64(minsCarried))
+	ts.append("second", uint64(secondsCarried))
+
+	return ts.b.String()
 }
--- internal/config/util_test.go
+++ internal/config/util_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"strings"
 	"testing"
+	"time"
 )
 
 const (
@@ -79,5 +80,98 @@ func TestFormatBaseAddressBadPort(t *testing.T) {
 
 	if strings.Compare(base, baseHTTPSDefaultPort) != 0 {
 		t.Error(baseHTTPSDefaultPort, base)
+	}
+}
+
+func TestFormatTime(t *testing.T) {
+	type args struct {
+		t time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Zero",
+			args: args{time.Second * 0},
+			want: "∞",
+		},
+		{
+			name: "1 Second",
+			args: args{time.Second},
+			want: "1 second",
+		},
+		{
+			name: "59 Seconds",
+			args: args{time.Second * 59},
+			want: "59 seconds",
+		},
+		{
+			name: "1 Minute",
+			args: args{time.Minute},
+			want: "1 minute",
+		},
+		{
+			name: "59 Minutes",
+			args: args{time.Minute * 59},
+			want: "59 minutes",
+		},
+		{
+			name: "1 Hour",
+			args: args{time.Minute * 60},
+			want: "1 hour",
+		},
+		{
+			name: "23 Hours",
+			args: args{time.Minute * 60 * 23},
+			want: "23 hours",
+		},
+		{
+			name: "1 Day",
+			args: args{time.Minute * 60 * 24},
+			want: "1 day",
+		},
+
+		{
+			name: "364 days",
+			args: args{time.Minute * 60 * 24 * 364},
+			want: "364 days",
+		},
+		{
+			name: "1 Year",
+			args: args{time.Minute * 60 * 24 * 365},
+			want: "1 year",
+		},
+
+		{
+			name: "2 Years",
+			args: args{time.Minute * 60 * 24 * 365 * 2},
+			want: "2 years",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FormatTime(tt.args.t); got != tt.want {
+				t.Errorf("FormatTime() = '%v', want '%v'", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkGetTimeStringLong(b *testing.B) {
+
+	testDuration := (time.Second * 1 * 59) + // 59 seconds
+		(time.Minute * 59) + // 59 minutes
+		(time.Minute * 60 * 23) + // 23 hours
+		(time.Minute * 60 * 24 * 364) + // 364 days
+		(time.Minute * 60 * 24 * 365 * 9) // 9 years
+
+	want := "9 years, 364 days, 23 hours, 59 minutes, 59 seconds"
+
+	for i := 0; i < b.N; i++ {
+		if got := FormatTime(testDuration); got != want {
+			b.Errorf("FormatTime() = '%v', want '%v'", got, want)
+		}
 	}
 }
